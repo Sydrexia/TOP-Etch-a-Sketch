70452017e3548765ca3e781aec3a8c403ac9e99f
Andrew Glach <andrew.glach@gmail.com>
2022-02-08 19:31:50 -0700

    Added click and drag drawing

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-const gameSpace = document.querySelector('#content')
+const gameSpace = document.querySelector('#content');
+const nodes = gameSpace.childNodes;
+let mouseDown = false;
+
+gameSpace.addEventListener('mousedown', () => {
+    mouseDown = true;
+})
+
+gameSpace.addEventListener('mouseup', () => {
+    mouseDown = false;
+})
+
+function colorIn(event) {
+    if (mouseDown === true) {
+        event.target.style['background-color'] = 'black';
+    }
+}
 
 for (i = 0; i < 256; i++) {
     const newDiv = document.createElement('div');
     newDiv.style.height = 'auto';
     newDiv.style.width = 'auto';
-    newDiv.addEventListener('mousedown', () => {
-        newDiv.addEventListener('mousemove', () => {
-            newDiv.style['background-color'] = 'black';
-        });
-    });
-    // newDiv.addEventListener('mouseup', () => {
-    //     newDiv.removeEventListener('mousemove');
-    // })
-    // newDiv.style.border = '2px solid red'
+    newDiv.classList.add('grid-div');
+    newDiv.addEventListener('mouseover', (colorIn));
     gameSpace.appendChild(newDiv);
 }
